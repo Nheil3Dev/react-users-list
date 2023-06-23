ce346656378516fbda1d1acb596841b31888e6b2
Nheil3Dev <98476350+Nheil3Dev@users.noreply.github.com>
2023-06-23 12:08:52 +0200

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
     'standard',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
     'eslint-config-prettier'
   ],
   parserOptions: {
